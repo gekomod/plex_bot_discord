24508fad355d8cbf225db6a9af10c0dc0ee2d108
gekomod <zaba141@o2.pl>
2024-09-03 11:05:03 -0700

    Update CommandsDiscord.js

--- functions/CommandsDiscord.js
+++ functions/CommandsDiscord.js
@@ -2,11 +2,25 @@ const refreshDatabase = require('./RefreshDatabase');
 const listMovies = require('./ListMovie');
 const actualListing = require('./ActualListing');
 const InfoServer = require('./InfoServer');
+const getMovieInfo = require('./getMovieInfo');
+
+function urlifyRecursiveFunc(str) {
+    if (str.length === 0) {
+        return '';
+    }
+    if (str[0] === ' ') {
+        return '%20' + urlifyRecursiveFunc(str.slice(1));
+    }
+    return str[0] + urlifyRecursiveFunc(str.slice(1));
+}
 
 function CommandsDiscord(messages) {
 	
 	// Commands Discord
 	messages.on('messageCreate', message => {
+		const prefix = '!';
+		const args = message.content.slice(prefix.length).trim().split(' ');
+		const command = args.shift().toLowerCase();
 		if (message.content === '!refresh') {
 			refreshDatabase();
 			message.channel.send('Baza danych została odświeżona.');
@@ -16,6 +30,11 @@ function CommandsDiscord(messages) {
 			actualListing(message.channel);
 		} else if (message.content === '!info') {
 			InfoServer();
+		} else if (command === 'getmovieinfo') {
+			const title = message.content.split(' ').slice(1).join(' ');
+			let ot = urlifyRecursiveFunc(title);
+			message.channel.send(`Command name: !getmovieinfo\nArguments: ${title}`);
+        		getMovieInfo(title, message.channel);
 		}
 
 	});
